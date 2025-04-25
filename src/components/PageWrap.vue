0d4f70ee82dd9e8d3a47bbae097e87db38cdafab
Tommaso Morganti <tommaso.morganti01@gmail.com>
2025-04-26 00:38:39 +0200

    feat: PageWrap component

---
+++ src/components/PageWrap.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import HeroPage from "./HeroPage.vue"
+
+defineProps<{
+  /**
+   * Hero page title
+   */
+  title: string
+  /**
+   * @optional Hero page subtitle, below the title
+   */
+  subtitle?: string
+  /**
+   * @optional Hero page tagline, above the title
+   */
+  tagline?: string
+  /**
+   * @optional URL for the hero image (Takes the image name from the `assets/images` folder)
+   */
+  imgSrc?: string
+}>()
+</script>
+
+<template>
+  <main>
+    <HeroPage
+      :title="title"
+      :url-img="imgSrc"
+      :subtitle="subtitle"
+      :tagline="tagline"
+    />
+    <article>
+      <slot />
+    </article>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  gap: 32px;
+  margin-bottom: 32px;
+}
+
+article {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 800px;
+  height: 100%;
+}
+</style>
--- src/pages/teacher.vue
+++ src/pages/teacher.vue
@@ -1,56 +1,52 @@
 <template>
-  <main>
-    <HeroPage
-      :title="teacher.name"
-      :subtitle="teacher.mantra"
-      tagline="The Teachers:"
-      url-img="Team.jpg"
-    />
-    <article>
-      <v-row>
-        <v-col cols="6">
-          <v-card>
-            <!--<v-card-title>Teacher Mantra</v-card-title>-->
-            <v-card-text>
-              <v-img src="../assets/images/Teacher1.jpg"></v-img>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="6">
-          <v-card>
-            <v-card-title>{{ teacher.name }}</v-card-title>
-            <v-card-subtitle>Who am I?</v-card-subtitle>
-            <v-card-text>
-              <p>{{ teacher.description }}</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+  <PageWrap
+    :title="teacher.name"
+    :subtitle="teacher.mantra"
+    tagline="The Teachers:"
+    img-src="Team.jpg"
+  >
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <!--<v-card-title>Teacher Mantra</v-card-title>-->
+          <v-card-text>
+            <v-img src="../assets/images/Teacher1.jpg"></v-img>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>{{ teacher.name }}</v-card-title>
+          <v-card-subtitle>Who am I?</v-card-subtitle>
+          <v-card-text>
+            <p>{{ teacher.description }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
 
-      <v-row>
-        <v-col cols="6">
-          <v-card>
-            <v-card-title>What we'll do</v-card-title>
-            <v-card-text>
-              <p>{{ teacher.description }}</p>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="6">
-          <v-card>
-            <!--<v-card-title>Teacher Mantra</v-card-title>-->
-            <v-card-text>
-              <v-img src="../assets/images/Teacher1.jpg"></v-img>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </article>
-  </main>
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>What we'll do</v-card-title>
+          <v-card-text>
+            <p>{{ teacher.description }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <!--<v-card-title>Teacher Mantra</v-card-title>-->
+          <v-card-text>
+            <v-img src="../assets/images/Teacher1.jpg"></v-img>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </PageWrap>
 </template>
 
 <script setup lang="ts">
-import HeroPage from "~/components/HeroPage.vue"
 interface Teacher {
   name: string
   description: string
