271254e187b96702b40e9133bbd17ace0aef367e
AlessiaBerni <102222336+AlessiaBerni@users.noreply.github.com>
2025-04-30 17:04:40 +0200

    Get & post test request ;) (#6)
    * example call
    
    * example post api call
    
    * get and Post requests
    
    ---------
    
    Co-authored-by: Tommaso Morganti <tommaso.morganti01@gmail.com>

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,13 @@
+<script setup lang="ts">
+import { TestComponent } from "#components"
+</script>
+
 <template>
   <div>
-    <test-component />
+    <TestComponent />
   </div>
 </template>
-<script lang="ts">
-import testComponent from "~/components/TestComponent.vue"
 
-export default {
-  components: {
-    testComponent,
-  },
-}
-</script>
 <style scoped>
 div {
   background-color: #f0f0f0;
