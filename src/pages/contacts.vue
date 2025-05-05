d85b34b222f5a5084366b2f2fca7cbf40fc4679e
Simone-Pedroni-Polimi <Simone.Pedroni+ElGhEmu@ext.luxottica.com>
2025-05-05 23:11:01 +0200

    Contacts page changes

--- src/pages/contacts.vue
+++ src/pages/contacts.vue
@@ -3,9 +3,10 @@
 
 <template>
   <PageWrap
-    :title="'Contacts'"
+    title="Contacts"
+    img-src="contact-page-banner.jpg"
   >
-    <section class="contact-option">
+    <section class="contact-section">
       <div class="row">
         <div class="column"></div>
         <div class="column">
@@ -27,13 +28,27 @@
       </div>
     </section>
 
-    <section class="map">
+    <section class="map-section">
       <div class="map-title">COME AND JOIN US</div>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d89509.5532925476!2d9.043172443359369!3d45.486450000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786c12dfe45ee8f%3A0x6528954343c34fd4!2sMilano%20Yoga%20Space!5e0!3m2!1sit!2sit!4v1746451557377!5m2!1sit!2sit" width="600" height="450" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
 
-    <section class="contact-form">
-      <form action="mailto:simone.pedroni@mail.polimi.it" method="post" enctype="text/plain">
+    <section class="form-section">
+      <form action="mailto:simone.pedroni@mail.polimi.it" method="post" enctype="text/plain" class="contact-form">
+        <label for="fname" class="form-label">Name</label> <br/>
+        <input id="fname" class="form-input" type="text" name="fname" placeholder="Value" required /> <br/><br/>
+
+        <label for="lname" class="form-label">Surname</label> <br/>
+        <input id="lname" class="form-input" type="text" name="lname" placeholder="Value" required /> <br/><br/>
+
+        <label for="mail" class="form-label">Mail</label> <br/>
+        <input id="mail" class="form-input" type="email" name="mail" placeholder="Value" required /> <br/><br/>
+
+        <label for="message" class="form-label">Message</label> <br/>
+        <textarea id="message" class="form-input" name="message" placeholder="Insert your message here" cols="70" rows="3" required></textarea> <br/><br/>
+
+        <input id="submit" type="submit" class="form-button" value="Submit"> <br/>
+        <input id="reset" type="reset" class="form-button" value="Reset">
       </form>
     </section>
   </PageWrap>
@@ -51,7 +66,7 @@
   text-align: center;
 }
 
-.map {
+.map-section {
   background-color: #F5F5F5;
   width: 100%;
   text-align: center;
@@ -62,6 +77,35 @@
 
 .map-title {
   margin-bottom: 20px;
+}
+
+.form-section {
+  margin-top: 40px;
+}
+
+.contact-form {
+  border: 1px solid #919090;
+  border-radius: 10px;
+  padding: 30px 20px;
+  width: 80%;
+  margin: auto;
+}
+
+.form-input {
+  border: 1px solid #919090;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 5px;
+  width: 100%;
+}
+
+.form-button {
+  width: 100%;
+  color: white;
+  background-color: black;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 5px 0;
 }
 
 </style>
