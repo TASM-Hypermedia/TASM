7eb84eeb9b3d47f09c44798b391350636e651fc1
Alessia Bernizzoni <alessia.bernizzoni@mail.polimi.it>
2025-05-07 09:05:25 +0200

    Creation common component ContentPreview.vue

--- src/pages/teacher.vue
+++ src/pages/teacher.vue
@@ -43,10 +43,25 @@
         </v-card>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col
+        v-for="n in 6"
+        :key="n"
+        class="pa-3"
+        cols="12"
+        sm="12"
+        md="6"
+        lg="4"
+      >
+        <content-preview :content-preview="contentPrev" />
+      </v-col>
+    </v-row>
   </PageWrap>
 </template>
 
 <script setup lang="ts">
+import ContentPreview from "~/components/ContentPreview.vue"
 interface Teacher {
   name: string
   description: string
@@ -68,4 +83,11 @@ withDefaults(
     }),
   }
 )
+
+const contentPrev = {
+  title: "Yoga for Beginners",
+  subtitle:
+    "Mother, mentor, poet, artist, volunteer, bestselling author and host of the Practice You Podcast, Elena Brower graduated Cornell University in 1992, designed textiles and apparel for almost a decade before shifting her focus to yoga, meditation, writing and art. Teaching asana since 1999, studying and practicing Zen meditation since 2020, she received the Buddhist Precepts from Roshi Joan Halifax at Upaya Zen Center in 2023. Now a candidate for Buddhist Chaplaincy, Elena offers her time in hospice and penitentiary settings. Her bestselling journals and books explore stages of life and listening. Her next book, Hold Nothing, will be published by Shambhala Publications in late 2025",
+  urlImg: "Team.jpg",
+}
 </script>
