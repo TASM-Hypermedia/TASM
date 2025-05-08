3f0a9c81e141d93cbb4f1c8b88f22914652bbd5f
Michele Mariani <michele2.mariani@mail.polimi.it>
2025-05-08 16:31:47 +0200

    Component dynamic generation (#10)
    * ActivityCard and CardGrid components added
    
    * Card Grid added, Activities page added
    
    * Cards with images
    
    * Our Team page created, teacher card created, activity tag created
    
    * New teacher card implementation
    
    * Dynamic component card generation
    
    ---------
    
    Co-authored-by: toto04 <tommaso.morganti01@gmail.com>

--- src/pages/activities.vue
+++ src/pages/activities.vue
@@ -1,24 +1,45 @@
 <template>
-    <div>
-        <card-grid/>
-    </div>
+    <page-wrap :title="'Activities'">
+        <card-grid :length="activitiesList.length">
+            <template #card="{ index }">
+                <activity-card :activity-prop="activitiesList[index]" />
+            </template>
+        </card-grid>
+    </page-wrap>
 </template>
 
-<script lang="ts">
-    import CardGrid from "~/components/CardGrid.vue";
-
-    export default {
-        components: {
-            CardGrid,
+<script setup lang="ts">
+    
+    const activitiesList = [
+        {
+            title: "Meditazione",
+            image: "https://placehold.co/400x400",
         },
-    }
+        {
+            title: "Mindfulness",
+            image: "https://placehold.co/400x400",
+        },
+        {
+            title: "Rituale",
+            image: "https://placehold.co/400x400",
+        },
+        {
+            title: "Better sleep",
+            image: "https://placehold.co/400x400",
+        },
+        {
+            title: "Golden Hour Sunset",
+            image: "https://placehold.co/400x400",
+        },
+    ];
 
 </script>
+
 <style scoped>
-div {
-    background-color: #f0f0f0;
-    flex: 1;
-    width: 100%;
-}
+    div {
+        background-color: #f0f0f0;
+        flex: 1;
+        width: 100%;
+    }
 
 </style>
