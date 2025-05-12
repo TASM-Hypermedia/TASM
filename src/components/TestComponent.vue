16d46b8dafffd39b2dffa31fcd662223da316f7e
Alessia Bernizzoni <alessia.bernizzoni@mail.polimi.it>
2025-05-12 10:28:06 +0200

    Creation AllTeachers page with call to db

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <teacher :teacher="teacherInfo" />
+    <all-teachers></all-teachers>
+    <!--<teacher :teacher="teacherInfo" />-->
   </div>
 </template>
 
 <script setup lang="ts">
-import teacher from "../pages/teacher.vue"
-import { useAPI } from "~/composables/useAPI"
+//import teacher from "../pages/teacher.vue"
+import allTeachers from "~/pages/allTeachers.vue"
+//import { useAPI } from "~/composables/useAPI"
 
 // Example GET API call
-const { data } = await useAPI<{ id: number; name: string }[]>("/getData")
+/*const { data } = await useAPI<{ id: number; name: string }[]>("/getData")
 
 if (!data.value || data.value.length === 0) {
   throw new Error("No data found")
@@ -32,8 +34,7 @@ const { data: dataPost } = await useAPI<{
 if (!dataPost.value) {
   throw new Error("No data found")
 }
-const namePost = dataPost.value.Name
-
+//const namePost = dataPost.value.Name
 const teacherInfo = {
   name: namePost,
   description: dataPost.value.Description,
@@ -41,5 +42,5 @@ const teacherInfo = {
   //"Sono Sara Morelli, insegnante di yoga certificata e appassionata di benessere olistico. La mia missione è guidarti in un viaggio di trasformazione attraverso il respiro, il movimento e la consapevolezza. Ho scoperto lo yoga durante un periodo di forte stress lavorativo e, da allora, non l'ho più abbandonato. Dopo anni di studio e pratica in India, Thailandia e Italia, ho deciso di condividere questa meravigliosa disciplina con chiunque voglia ritrovare equilibrio e serenità nella propria vita.",
   mantra: dataPost.value.Mantra,
   mantraSubtitle: "Inspiring minds, shaping futures.",
-}
+}*/
 </script>
