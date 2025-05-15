4f0bf952543164c4982728fe41af615fab3101fc
Simone-Pedroni-Polimi <87439991+Simone-Pedroni-Polimi@users.noreply.github.com>
2025-05-15 17:15:57 +0200

    pepe (#16)
    Co-authored-by: Simone-Pedroni-Polimi <Simone.Pedroni+ElGhEmu@ext.luxottica.com>
    Co-authored-by: Tommaso Morganti <tommaso.morganti01@gmail.com>
    Co-authored-by: michele-mariani <michele2.mariani@mail.polimi.it>

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,3 +21,12 @@ export type Faq = {
   question: string,
   answer: string,
 };
+
+export type ContentCard = {
+  title: string,
+  subtitle?: string,
+  description?: string,
+  imgUrl?: string,
+  altDescription?: string,
+  imageOnTheRight: boolean,
+}
