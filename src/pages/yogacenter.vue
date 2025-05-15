6ed814d9112f1a65354b767de917b08dfbe3eed3
Simone-Pedroni-Polimi <Simone.Pedroni+ElGhEmu@ext.luxottica.com>
2025-05-15 19:19:28 +0200

    YogaCenter page draft

---
+++ src/pages/yogacenter.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+const { data, error } = await useAPI<{
+  YogaCenterId: number
+  Title: string
+  Subtitle: string
+  LongDescription: string
+  Rooms: Array<{
+      Name: string;
+      Text: string;
+      UrlImage: string;
+  }>
+}>("/getAllRooms", {
+  method: "GET"
+})
+
+if (error.value || !data.value) {
+  console.error("Error in loading yoga center:", error.value)
+  throw new Error("Yoga center not found")
+}
+
+console.log(data)
+
+</script>
+
+<template>
+  <PageWrap
+    title="Yoga Center"
+  >
+    <div>
+      {{data?.LongDescription}}
+    </div>
+  </PageWrap>
+</template>
+
+<style scoped>
+
+</style>
