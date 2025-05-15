4b43b010e98cc4af95b0524b5081e15ba0a14074
Tommaso Morganti <tommaso.morganti01@gmail.com>
2025-05-15 23:13:43 +0200

    Discard changes to src/pages/activities/index.vue

--- src/pages/activities/index.vue
+++ src/pages/activities/index.vue
@@ -1,49 +1,57 @@
 <template>
-  <page-wrap :title="'Activities'">
-    <template v-if="activitiesList.length > 0">
-      <card-grid :length="activitiesList.length">
-        <template #card="{ index }">
-          <activity-card :activity-prop="activitiesList[index]" />
+    <page-wrap :title="'Activities'">
+        <template v-if="activitiesList.length > 0">
+            
+            <card-grid :length="activitiesList.length">
+                <template #card="{ index }">
+                    <activity-card :activity-prop="activitiesList[index]" />
+                </template>
+            </card-grid> 
+            
         </template>
-      </card-grid>
-    </template>
-    <template v-else>
-      <div class="empty-state">No activities available at the moment.</div>
-    </template>
-  </page-wrap>
+        <template v-else>
+
+            <div class="empty-state">
+                No activities available at the moment.
+            </div>
+
+        </template>
+    </page-wrap>
 </template>
 
 <script setup lang="ts">
-import type { Activity } from "~/types"
+    import type { Activity } from '~/types';
+    
+    const activitiesList: Activity[]= [
+        // {
+        //     title: "Meditazione",
+        //     image: "https://placehold.co/400x400",
+        // },
+        // {
+        //     title: "Mindfulness",
+        //     image: "https://placehold.co/400x400",
+        // },
+        // {
+        //     title: "Rituale",
+        //     image: "https://placehold.co/400x400",
+        // },
+        // {
+        //     title: "Better sleep",
+        //     image: "https://placehold.co/400x400",
+        // },
+        // {
+        //     title: "Golden Hour Sunset",
+        //     image: "https://placehold.co/400x400",
+        // },
+    ];
 
-const activitiesList: Activity[] = [
-  // {
-  //     title: "Meditazione",
-  //     image: "https://placehold.co/400x400",
-  // },
-  // {
-  //     title: "Mindfulness",
-  //     image: "https://placehold.co/400x400",
-  // },
-  // {
-  //     title: "Rituale",
-  //     image: "https://placehold.co/400x400",
-  // },
-  // {
-  //     title: "Better sleep",
-  //     image: "https://placehold.co/400x400",
-  // },
-  // {
-  //     title: "Golden Hour Sunset",
-  //     image: "https://placehold.co/400x400",
-  // },
-]
 </script>
 
 <style scoped>
-div {
-  background-color: #f0f0f0;
-  flex: 1;
-  width: 100%;
-}
+    div {
+        background-color: #f0f0f0;
+        flex: 1;
+        width: 100%;
+    }
+
 </style>
