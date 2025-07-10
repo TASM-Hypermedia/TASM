160b775a834bbba0dd33bccc359f1b8d0e2f26e5
Alessia Bernizzoni <alessia.bernizzoni@mail.polimi.it>
2025-07-10 22:01:32 +0200

    ciao ;)

---
+++ src/components/home/BenefitsContentCard.vue
@@ -0,0 +1,157 @@
+<script setup lang="ts">
+import SvgBlob from "~/assets/images/homepage/Blob-Fill.svg"
+import SvgBlobOutline from "~/assets/images/homepage/Blob-outline.svg"
+import { motion } from "motion-v"
+//import { ref, computed } from "vue" //ref, computedimport { } from "vue"
+
+const { benefitsProp } = defineProps<{
+  benefitsProp: {
+    title: string
+    description: string
+    image: string
+    position: number
+  }
+}>()
+
+const variations = {
+  hidden: {
+    scale: 1,
+    opacity: 0,
+    x: -150,
+  },
+  shown: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
+  },
+  hover: {
+    scale: 1.015,
+  },
+}
+</script>
+<template>
+  <motion.div
+    while-hover="hover"
+    initial="hidden"
+    animate="shown"
+    :variants="variations"
+    style="
+      flex: 1;
+      padding: 30px;
+      /* background: #bde0fe; */
+      /* overflow: hidden; */
+      border-radius: 100px;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      display: inline-flex;
+      position: relative;
+    "
+  >
+    <div style="display: flex; flex-direction: column; gap: 20px">
+      <div>
+        <div
+          style="
+            align-self: stretch;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 50px;
+            display: flex;
+            position: relative;
+          "
+        >
+          <div
+            style="justify-content: center; align-items: center; display: flex"
+          >
+            <svg-blob class="svg-blob" />
+            <svg-blob-outline class="svg-blob-outline" />
+            <img
+              class="teacher_image"
+              style="
+                width: 50%;
+                height: 50%;
+                object-fit: contain;
+                justify-content: center;
+                align-items: center;
+                scale: 0.8;
+                /*border-radius: 7px;*/
+              "
+              :src="'/images/icons/benefits/' + benefitsProp.image"
+              :alt="benefitsProp.title"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div class="cardTitle title">
+          {{ benefitsProp.title }}
+        </div>
+      </div>
+
+      <div>
+        <div class="cardDescription body-text">
+          {{ benefitsProp.description }}
+        </div>
+      </div>
+    </div>
+  </motion.div>
+</template>
+
+<style scoped>
+.cardTitle {
+  padding-top: 5%;
+  align-self: stretch;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  color: black;
+  /*font-size: 48px;
+  line-height: 57.6px;*/
+  font-family: Inter;
+  font-weight: 700;
+  word-wrap: break-word;
+}
+
+.cardDescription {
+  align-self: stretch;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  color: black;
+  /*font-size: 32px;
+  line-height: 38.4px;*/
+  font-family: Inter;
+  font-weight: 400;
+  height: 100%;
+  word-wrap: break-word;
+}
+.benefit-card {
+  position: relative;
+}
+.svg-blob {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: fit-content;
+  height: fit-content;
+  z-index: -1;
+  transform: translate(-50%, -50%) scale(1.7);
+}
+.svg-blob-outline {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: fit-content;
+  height: fit-content;
+  z-index: -1;
+  transform: translate(-30%, -40%) scale(1.7);
+  opacity: 0.5;
+}
+/* div {
+  border: 1px solid black;
+} */
+</style>
---
+++ src/components/home/BenefitsWrapper.vue
@@ -0,0 +1,177 @@
+<script setup lang="ts">
+// import SvgBottom from "~/assets/images/homepage/Bottom-wrapper-benefits.svg"
+// import SvgTop from "~/assets/images/homepage/Top-wrapper-benefits.svg"
+
+import SvgWave from "~/assets/images/homepage/wave-outline2.svg"
+import { onMounted } from "vue"
+import { motion } from "motion-v"
+const benefits = [
+  {
+    title: "CONNECTION",
+    description: "Reconnect with yourself, others, and the world around you",
+    image: "Connessione.svg",
+    position: 0,
+  },
+  {
+    title: "CLARITY",
+    description:
+      "Find mental focus and inner stillness through breath and mindful movement",
+    image: "Lampadina.svg",
+    position: 1,
+  },
+  {
+    title: "STRENGTH",
+    description: "Build physical resilience and emotional balance",
+    image: "Forza.svg",
+    position: 2,
+  },
+  {
+    title: "FREEDOM",
+    description: "Release tension in the body and let go of limiting thoughts",
+    image: "Liberta.svg",
+    position: 3,
+  },
+]
+onMounted(() => {
+  console.log(window.innerWidth)
+  clientWidth.value = window.innerWidth
+
+  window.addEventListener("resize", () => {
+    clientWidth.value = window.innerWidth
+  })
+})
+
+const clientWidth = ref(0)
+//window.innerWidth
+
+console.log(clientWidth)
+
+const ratioTop = 1200 / 178
+
+const ratioBottom = 1200 / 178
+
+const topHeight = computed(() => -clientWidth.value / ratioTop)
+const bottomHeight = computed(() => -clientWidth.value / ratioBottom)
+console.log(topHeight, bottomHeight)
+</script>
+
+<template>
+  <div class="container-benefits">
+    <div class="sheet"></div>
+    <!-- <SvgBottom
+      class="svg-imageBottom"
+      :style="{ bottom: bottomHeight + 'px', width: '100vw' }"
+    />
+    <SvgTop
+      class="svg-imageTop"
+      :style="{ top: topHeight + 'px', width: '100vw' }"
+    /> -->
+
+    <SvgWave
+      class="svg-imageTop"
+      :style="{ top: topHeight + 'px', width: '100vw' }"
+    /><SvgWave
+      class="svg-imageTop"
+      :style="{ top: topHeight - topHeight * 0.2 + 'px', width: '100vw' }"
+    /><SvgWave
+      class="svg-imageTop"
+      :style="{ top: topHeight - topHeight * 0.35 + 'px', width: '100vw' }"
+    />
+
+    <!-- <SvgWave
+      class="svg-imageBottom"
+      :style="{ bottom: bottomHeight + 'px', width: '100vw' }"
+    />
+    <SvgWave
+      class="svg-imageBottom"
+      :style="{ bottom: bottomHeight - topHeight * 0.2 + 'px', width: '100vw' }"
+    />
+    <SvgWave
+      class="svg-imageBottom"
+      :style="{
+        bottom: bottomHeight - topHeight * 0.35 + 'px',
+        width: '100vw',
+      }"
+    /> -->
+    <div
+      style="
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        z-index: 1;
+        align-items: center;
+        padding: 50px;
+        position: relative;
+      "
+    >
+      <div style="display: flex">
+        <AnimatedTitle title="BENEFITS" />
+      </div>
+
+      <motion.div
+        class="benefits-cards-wrapper"
+        initial="hidden"
+        animate="shown"
+        :transition="{
+          duration: 0.2,
+          staggerChildren: 0.2,
+        }"
+      >
+        <BenefitsContentCard
+          v-for="i in benefits.length || 0"
+          :key="i"
+          style="flex: 1"
+          :benefits-prop="benefits[i - 1]"
+        />
+      </motion.div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-benefits {
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  margin-bottom: 300px;
+  margin-top: 250px;
+}
+
+.svg-imageBottom {
+  width: 100vw;
+  /*height: auto;*/
+  left: 0;
+  /*bottom: 0;*/
+  position: absolute;
+}
+.svg-imageTop {
+  width: 100vw;
+  /*height: auto;*/
+  left: 0;
+  /*top: 0px;*/
+  position: absolute;
+}
+.sheet {
+  width: 100vw;
+  bottom: 0;
+  top: -20px;
+  left: 0;
+  /*top: 200px;*/
+  position: absolute;
+  background-color: #f0e2f9;
+}
+.benefits-cards-wrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  gap: 20px;
+}
+</style>
